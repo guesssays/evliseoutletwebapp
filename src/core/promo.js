21eb501b9e35219ae249eb94c2e49fd4aa47f1d7
Danil Redko <danilred2002@gmail.com>
2025-11-01 16:29:33 +0500

    288

--- src/core/promo.js
+++ src/core/promo.js
@@ -3,7 +3,7 @@ import { state } from './state.js';
 
 function defaults() {
   return {
-    enabled: false,
+    enabled: true,
     slug: 'newyear-2026',
     title: 'Новогодняя акция',
     subtitle: 'скидки и x2 кэшбек',
